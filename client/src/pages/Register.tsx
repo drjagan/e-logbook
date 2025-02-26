f455da9c9af28acd60e5605dcefad3327735f1fa
Dr Jagan Mohan <dr@jm.gl>
2025-02-26 09:24:31 +0700

    Added Departments as per NMC, Feb 2025

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -15,17 +15,28 @@ import { useAppDispatch, useAppSelector } from '../store/hooks';
 import { register, clearError } from '../store/slices/authSlice';
 
 const departments = [
-  'Internal Medicine',
-  'Surgery',
-  'Pediatrics',
-  'Obstetrics & Gynecology',
-  'Psychiatry',
-  'Family Medicine',
-  'Emergency Medicine',
-  'Anesthesiology',
-  'Radiology',
-  'Pathology',
-  'Other',
+  'Anatomy (M.D.)',
+  'Anaesthesiology (M.D.)',
+  'Biochemistry (M.D.)',
+  'Community Medicine (M.D.)',
+  'Dermatology, Venereology and Leprosy (M.D.)',
+  'Emergency Medicine (M.D.)',
+  'Family Medicine (M.D.)',
+  'Forensic Medicine and Toxicology (M.D.)',
+  'General Medicine (M.D.)',
+  'Microbiology (M.D.)',
+  'Pediatrics (M.D.)',
+  'Pathology (M.D.)',
+  'Pharmacology (M.D.)', 
+  'Physiology (M.D.)',
+  'Psychiatry (M.D.)',
+  'Radio-diagnosis (M.D.)',
+  'Respiratory Medicine (M.D.)',
+  'General Surgery (M.S.)',
+  'Obstetrics and Gynecology (M.S.)',
+  'Ophthalmology (M.S.)',
+  'Orthopaedics (M.S.)',
+  'Otorhinolaryngology (M.S.)'
 ];
 
 const Register = () => {
